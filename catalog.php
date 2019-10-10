e5aa877c7427da1ce8ba509206d7997c1213b6bf
Daniel H <dhenkes95@icloud.com>
2019-10-10 17:14:01 +0000

    Add initial files

---
+++ catalog.php
@@ -0,0 +1,36 @@
+<?php
+$catalog = array(
+  "Design Patterns",
+  "Forrest Gump",
+  "Beethoven"
+);
+
+$pageTitle = "Full Catalog";
+$section = null;
+
+if (isset($_GET["cat"])) {
+if ($_GET["cat"] == "books") {
+    $pageTitle = "Books";
+    $section = "books";
+} else if ($_GET["cat"] == "movies") {
+    $pageTitle = "Movies";
+    $section = "movies";
+} else if ($_GET["cat"] == "music") {
+    $pageTitle = "Music";
+    $section = "music";
+}
+}
+include("include/header.php"); ?>
+
+<div class="section catalog page">
+  <div class="wrapper">
+  <h1><?php echo $pageTitle; ?></h1>
+    <ul>
+      <?php foreach($catalog as $item) {
+          echo "<li>" . $item . "</li>";
+      } ?>
+    </ul>
+</div>
+  </div>
+  
+<?php include("include/footer.php"); ?>
